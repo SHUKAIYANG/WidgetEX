d2633e508db73c525585b3632c39dd4e0394c4e9
SK Yang <rock5215@yahoo.com.tw>
2020-03-15 22:43:56 +0800

    WidgetEX chang to practice intent.

--- app/src/main/java/com/example/widgetex/MainActivity.java
+++ app/src/main/java/com/example/widgetex/MainActivity.java
@@ -2,7 +2,10 @@ package com.example.widgetex;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.ImageButton;
 import android.widget.ImageView;
@@ -10,14 +13,19 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    private static final String TAG = "main";
     private ImageButton imagebutton1, imagebutton2, imagebutton3, imagebutton4, imagebutton5, imagebutton6, imagebutton7, imagebutton8;
     private ImageView imageViewPic;
     private TextView textViewresult;
+    private Context context;
+    private String name;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        context = this;
 
         // 設定 imagebutton物件
         imagebutton1 = (ImageButton) findViewById(R.id.imageButton1);
@@ -29,13 +37,6 @@ public class MainActivity extends AppCompatActivity {
         imagebutton7 = (ImageButton) findViewById(R.id.imageButton7);
         imagebutton8 = (ImageButton) findViewById(R.id.imageButton8);
 
-        imageViewPic = (ImageView) findViewById(R.id.imageView_pic);
-
-        imageViewPic.setVisibility(View.INVISIBLE);
-
-        textViewresult = (TextView) findViewById(R.id.textView_result);
-
-        textViewresult.setText("");
 
         // 監聽 imagebutton
         imagebutton1.setOnClickListener(new MyClick());
@@ -53,55 +54,83 @@ public class MainActivity extends AppCompatActivity {
     private class MyClick implements View.OnClickListener {
         @Override
         public void onClick(View v) {
+
+            Intent intent = new Intent(context, DisplayActivity.class);
+
             switch (v.getId()){
                 case R.id.imageButton1:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_1_b);
-                    textViewresult.setText("This is image 1.");
+                    name = getResources().getString(R.string.imageButton1);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_1_b);
+                    intent.putExtra("text", R.string.imageButton1_info);
+
                     break;
 
                 case R.id.imageButton2:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_2_b);
-                    textViewresult.setText("This is image 2.");
+                    name = getResources().getString(R.string.imageButton2);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_2_b);
+                    intent.putExtra("text", R.string.imageButton2_info);
+
                     break;
 
                 case R.id.imageButton3:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_3_b);
-                    textViewresult.setText("This is image 3.");
+                    name = getResources().getString(R.string.imageButton3);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_3_b);
+                    intent.putExtra("text", R.string.imageButton3_info);
+
                     break;
 
                 case R.id.imageButton4:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_4_b);
-                    textViewresult.setText("This is image 4.");
+                    name = getResources().getString(R.string.imageButton4);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_4_b);
+                    intent.putExtra("text", R.string.imageButton4_info);
+
                     break;
 
                 case R.id.imageButton5:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_5_b);
-                    textViewresult.setText("This is image 5.");
+                    name = getResources().getString(R.string.imageButton5);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_5_b);
+                    intent.putExtra("text", R.string.imageButton5_info);
+
                     break;
 
                 case R.id.imageButton6:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_6_b);
-                    textViewresult.setText("This is image 6.");
+                    name = getResources().getString(R.string.imageButton6);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_6_b);
+                    intent.putExtra("text", R.string.imageButton6_info);
                     break;
 
                 case R.id.imageButton7:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_7_b);
-                    textViewresult.setText("This is image 7.");
+                    name = getResources().getString(R.string.imageButton7);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_7_b);
+                    intent.putExtra("text", R.string.imageButton7_info);
+
                     break;
 
                 case R.id.imageButton8:
-                    imageViewPic.setVisibility(View.VISIBLE);
-                    imageViewPic.setImageResource(R.drawable.pic_8_b);
-                    textViewresult.setText("This is image 8.");
+                    name = getResources().getString(R.string.imageButton8);
+                    Log.d(TAG, "name = " + name);
+                    intent.putExtra("title", name);
+                    intent.putExtra("image", R.drawable.pic_8_b);
+                    intent.putExtra("text", R.string.imageButton8_info);
+
                     break;
             }
+
+            startActivity(intent);
 
         }
     }
